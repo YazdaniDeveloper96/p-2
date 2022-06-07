7996d87341d0bf777441064c335d67c7f7609366
Mohammad Reza <mohammad.r.yazdaniyan@gmail.com>
2022-06-07 22:03:26 +0430

    update read data from api (fun)

--- DAL/read_data_from_api.py
+++ DAL/read_data_from_api.py
@@ -6,6 +6,7 @@ sys.path.insert(0,"C:/Users/MohammadReza/Desktop/p-2")
 import json
 import requests
 from external_modules.url_error_handling import Url_Error_Handling
+from external_modules.city_error_handling import City_Error_Handling
 #-----------------------------------------------------------
 def read_api_data(cityname):
     result_list=[]
@@ -14,10 +15,17 @@ def read_api_data(cityname):
         for item in req.json():
             if item['state-province']==cityname.capitalize():
                 result_list.append({'state-province':item['state-province'],"name":item["name"],"web_pages":item["web_pages"][0]})
+            else:
+                raise City_Error_Handling('your city is not found')
         return result_list
     else:
         raise Url_Error_Handling ('our server is out of order')
 
-
-for item in read_api_data('tehran'):
-    print(item)
+if __name__=='__main__':
+    try:
+        for item in read_api_data('mashhad'):
+            print(item)
+    except Url_Error_Handling as error:
+            print(error)
+    except City_Error_Handling as error:
+            print(error)

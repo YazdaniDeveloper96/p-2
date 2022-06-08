04b5e1c05dfc919895b7cb365c5c4812d6b461cb
Mohammad Reza <mohammad.r.yazdaniyan@gmail.com>
2022-06-08 17:10:01 +0430

    update V1.0.2

--- external_modules/play_song.py
+++ external_modules/play_song.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0,"C:/Users/MohammadReza/Desktop/p-2")
 #-----------------------------------module
 import pygame     # ok
 #-----------------------------------------
@@ -7,7 +9,7 @@ import pygame     # ok
 def playSong(value='play'):
     if value=='play':
         pygame.mixer.init()
-        pygame.mixer.music.load('external_modules/music/0bikalam download1music.ir (1).mp3')
+        pygame.mixer.music.load('music/0bikalam download1music.ir (1).mp3')
         pygame.mixer.music.play(loops=2)
     else:
         pygame.mixer.music.stop()

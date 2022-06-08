899a1b0c94d8030578c44cb47ab297182a919aa8
Mohammad Reza <mohammad.r.yazdaniyan@gmail.com>
2022-06-08 17:05:31 +0430

    update play song (fun)

--- external_modules/play_song.py
+++ external_modules/play_song.py
@@ -7,7 +7,7 @@ import pygame     # ok
 def playSong(value='play'):
     if value=='play':
         pygame.mixer.init()
-        pygame.mixer.music.load('external_modules/music/Music relax Musiceto (10).mp3')
+        pygame.mixer.music.load('external_modules/music/0bikalam download1music.ir (1).mp3')
         pygame.mixer.music.play(loops=2)
     else:
         pygame.mixer.music.stop()
